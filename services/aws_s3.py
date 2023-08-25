229fb82ed8a99bf8cafbaaea19ceb97df6e5646a
Iyad ELwy <iyadelwy1@gmail.com>
2023-08-25 18:32:38 +0300

    feat: add s3 resource and get the client from the resource

--- services/aws_s3.py
+++ services/aws_s3.py
@@ -3,12 +3,13 @@ from decouple import config
 from io import BytesIO
 import json
 
-
-s3_client = boto3.client(
+s3_resource = boto3.resource(
     's3',
     aws_access_key_id=config('AWS_ACCESS_KEY'),
     aws_secret_access_key=config('AWS_SECRET_ACCESS_KEY'),
     region_name=config('REGION'))
+
+s3_client = s3_resource.meta.client
 
 
 def save_titles_url_to_s3(data_as_list, file_name):
